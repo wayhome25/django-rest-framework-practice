d1303c2f9ff2b16cbb378955b1fe4ec5414c6188
Lee HyunJoo <wayhome250@gmail.com>
2018-01-14 15:01:45 +0900

    ep03 Post API, generics 활용

--- ep03/views.py
+++ ep03/views.py
@@ -1,34 +1,15 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status, mixins, generics
+from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from ep03.models import Post
 from ep03.serializers import PostSerializer
 
 
-class PostListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class PostListAPIView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-
-class PostDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
-                        generics.GenericAPIView):
+class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
