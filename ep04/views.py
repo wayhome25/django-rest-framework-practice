1495874bb0830235e1f209a18c77511b71eaba5e
Lee HyunJoo <wayhome250@gmail.com>
2018-01-21 15:39:11 +0900

    ModelViewSet을 통해 creaate, update, partial update, destroy 구현

--- ep04/views.py
+++ ep04/views.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from django.shortcuts import render
 
 from ep04.models import Post
 from ep04.serializers import PostSerializer
 
 
-class PostViewSet(ReadOnlyModelViewSet):
+class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 post_list = PostViewSet.as_view({
     'get': 'list',
+    'post': 'create',
 })
 
 post_detail = PostViewSet.as_view({
     'get': 'retrieve',
+    'post': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
 })
