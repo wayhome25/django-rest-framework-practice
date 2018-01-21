f5602fe5089de07e9bdf73bec5ee099286404fa0
Lee HyunJoo <wayhome250@gmail.com>
2018-01-21 15:24:32 +0900

    ViewSet을 통해 post_list 뷰 구현

--- ep04/urls.py
+++ ep04/urls.py
@@ -1,7 +1,9 @@
 from django.urls.conf import path
 
+from ep04 import views
+
 app_name = 'ep04'
 
 urlpatterns = [
-
+    path('post/', views.post_list),
 ]
--- ep04/views.py
+++ ep04/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from ep04.models import Post
+from ep04.serializers import PostSerializer
+
+
+class PostViewSet(ReadOnlyModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+post_list = PostViewSet.as_view({
+    'get': 'list',
+})
